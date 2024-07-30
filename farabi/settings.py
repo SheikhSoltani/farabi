ed2af855acc89af98014589f93f0fff5b9b0d477
Allldo <dotan2.maks@mail.ru>
2024-07-30 14:52:42 +0500

    settings

--- farabi/settings.py
+++ farabi/settings.py
@@ -28,7 +28,7 @@ CART_SESSION_ID = 'cart'
 # CORS_ORIGIN_ALLOW_ALL = True
 # CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000"]
 SESSION_COOKIE_AGE = 42600
-ALLOWED_HOSTS = ['api.idealf.kz','idealf.kz']
+ALLOWED_HOSTS = ['127.0.0.1', 'api.idealf.kz','idealf.kz']
 some_data = True
 # CORS_ALLOWED_ORIGINS = [
 #     "http://127.0.0.1:8080",
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
