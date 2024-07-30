680ff9191684dd9b8823a173afc4eb82ff4e1ab1
Allldo <dotan2.maks@mail.ru>
2024-07-30 16:42:57 +0500

    func

--- front/src/js/cookie.js
+++ front/src/js/cookie.js
@@ -1,11 +1,16 @@
 function getCookie(name) {
   let cookieValue = null;
+  console.log('CALLED COOKIE GETTER')
   if (document.cookie && document.cookie !== '') {
+      console.log('IN FIRST IF')
     const cookies = document.cookie.split(';');
+      console.log(cookies)
     for (let i = 0; i < cookies.length; i++) {
+        console.log(cookies)
       const cookie = cookies[i].trim();
       // Если cookie начинается с имени, то извлекаем значение
       if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          console.log(cookieValue)
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
         break;
       }
