bb4c9b67cb78e63dea32e6d760d93a559b9204a9
Allldo <dotan2.maks@mail.ru>
2024-07-30 15:32:47 +0500

    qwe

--- front/src/js/axios.js
+++ front/src/js/axios.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  xsrfCookieName: 'csrftoken',
-  xsrfHeaderName: "X-CSRFTOKEN",
   baseURL: 'https://api.idealf.kz/',
   withCredentials: true,
 });
--- front/src/js/cookie.js
+++ front/src/js/cookie.js
@@ -1,9 +1,24 @@
-
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Если cookie начинается с имени, то извлекаем значение
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
     export function getConfig(t) {
      const config = {
         headers: {
         'Content-Type': t,
         'Access-Control-Allow-Origin': '*',
+        'X-CSRFTOKEN': getCookie('csrftoken')
         }
     };
     return config;
