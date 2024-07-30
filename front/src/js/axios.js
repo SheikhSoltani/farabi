1d13fc42eb83ffacf7aaabf254ef6a86f1c1900b
Allldo <dotan2.maks@mail.ru>
2024-07-30 15:29:23 +0500

    qwe

--- front/src/js/axios.js
+++ front/src/js/axios.js
@@ -1,27 +1,10 @@
 import axios from 'axios';
-function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      // Если cookie начинается с имени, то извлекаем значение
-      if (cookie.substring(0, name.length + 1) === (name + '=')) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
-    }
-  }
-  return cookieValue;
-}
 
-const csrfToken = getCookie('csrftoken');
 const axiosInstance = axios.create({
+  xsrfCookieName: 'csrftoken',
+  xsrfHeaderName: "X-CSRFTOKEN",
   baseURL: 'https://api.idealf.kz/',
-  withCredentials: true, // Replace with your actual base URL
-  headers: {
-      'X-CSRFToken': csrfToken
-    }
+  withCredentials: true,
 });
 
 export default axiosInstance;
--- front/src/js/cookie.js
+++ front/src/js/cookie.js
@@ -1,14 +1,9 @@
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-    }
+
     export function getConfig(t) {
      const config = {
         headers: {
         'Content-Type': t,
         'Access-Control-Allow-Origin': '*',
-        'X-CSRFTOKEN': getCookie('csrftoken')
         }
     };
     return config;
